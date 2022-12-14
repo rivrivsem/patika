dd80cd6a1a8d28a891ca242045f61a63ffae21b3
rivrivsem <smrdnmz43@gmail.com>
2022-12-15 00:53:23 +0100

    Create BInarySearchTree.java

---
+++ src/main/java/patika/BInarySearchTree.java
@@ -0,0 +1,34 @@
+package patika;
+
+public class BInarySearchTree {
+    //www.patika.dev
+    //@author semra
+
+    /*
+    Proje 3
+
+
+    [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+    verilen dizinin ilk elemanı 7 olacak şekilde kök düğüm olarak seçeriz,
+    daha sonra, dizinin geri kalan elemanları karşılaştırılarak yerleştiririz
+
+
+          7
+          / \
+          5   8
+          / \
+          3   9
+          / \   \
+          1   6   4
+          /
+          2
+
+           Bu örnekte, kök düğüm 7'dir ve sol dallarda 5, sağ dallarda ise 8 olur.
+           8 düğümünün sol dalında 3 bulunur ve sağ dalında 9 bulunur.
+           3 düğümünün sol dalında 1 ve sağ dalında 6 bulunur.
+           9 düğümünün sağ dalında ise 4 bulunur ve 4 düğümünün sol dalında 2 bulunur.
+
+*/
+
+}
