518dd976e6f264a6bf686ce4439077eecaca67ff
rivrivsem <smrdnmz43@gmail.com>
2022-12-15 00:48:07 +0100

    alan odevi eklendi

---
+++ src/main/java/patika/DaireninAlaniVeCevresi.java
@@ -0,0 +1,31 @@
+package patika;
+
+import java.util.Scanner;
+
+public class DaireninAlaniVeCevresi {
+
+    public static void main(String[] args) {
+        /*
+        Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanı bulan programı yazınız.
+
+        𝜋 sayısını = 3.14 alınız.
+
+        Formül : (𝜋 * (r*r) * 𝛼) / 360
+         */
+
+        double pi = 3.14,r,a;
+
+        Scanner input =new Scanner(System.in);
+        System.out.println("yaricapi giriniz...");
+        r=input.nextDouble();
+        System.out.println("merkez acinin olcusunu giriniz");
+        a=input.nextDouble();
+
+        double daireDilimAlan = (pi*(r*r)*a)/360;
+        System.out.println("daireDilimAlan = " + daireDilimAlan);
+
+
+
+
+    }
+}
