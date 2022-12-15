9fe2e1ccb28b6a7d33b6bd296d9a45e73ed066df
rivrivsem <smrdnmz43@gmail.com>
2022-12-15 01:04:43 +0100

    degisiklikler

--- src/main/java/patika/VucutKitleIndex.java
+++ src/main/java/patika/VucutKitleIndex.java
@@ -3,17 +3,16 @@ package patika;
 import java.util.Scanner;
 
 public class VucutKitleIndex {
+
+    //www.patika.dev
+    //@author semra
+
     /*
     Vücut Kitle İndeksi Hesaplama
-    Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
-
-    Formül
-    Kilo (kg) / Boy(m) * Boy(m)
-
-    Çıktısı
-    Lütfen boyunuzu (metre cinsinde) giriniz : 1,72
-    Lütfen kilonuzu giriniz : 105
-    Vücut Kitle İndeksiniz : 35.49215792320173
+    Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın.
+    Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+      Formül
+      Kilo (kg) / Boy(m) * Boy(m)
      */
 
     public static void main(String[] args) {
