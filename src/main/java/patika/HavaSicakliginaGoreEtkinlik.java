8abcbcb690cc9e6a465830d18f7f813caec80f28
rivrivsem <smrdnmz43@gmail.com>
2022-12-16 14:05:46 +0100

    guncellendi

--- src/main/java/patika/HavaSicakliginaGoreEtkinlik.java
+++ src/main/java/patika/HavaSicakliginaGoreEtkinlik.java
@@ -3,27 +3,30 @@ package patika;
 import java.util.Scanner;
 
 public class HavaSicakliginaGoreEtkinlik {
-    
+
 
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         System.out.println("bugunun hava sicaklik derecesini giriniz...");
         double derece=input.nextDouble();
         
-        if (derece<5)
+        if (derece<5 && derece>-20)
         {
             System.out.println("kayak yapabilirsin");
         }
-        else if (derece>5 &&derece<16)
+        else if (derece>5 && derece<16)
         {
             System.out.println("Sinema guzel fikir olabilir...");
         }
         else if (derece>15 && derece<26)
         {
             System.out.println("cikar mangalii cikarr... piknige gidiyoruz");
-        } else if (derece>25)
+        } else if (derece>25 && derece<50)
         {
             System.out.println("Yüzmek guzel fikir olabilir...");
+        }else
+        {
+            System.out.println("bahhh isine kardisiimm bu derecede birsey yapilmaz,hayatta kalmaya cali$ :))");
         }
 
     }
