dde2c8aaa088062074024b831aa1ae9c388bd41c
rivrivsem <smrdnmz43@gmail.com>
2022-12-19 00:26:57 +0100

    artik yil eklendi

---
+++ src/main/java/patika/ArtıkYilHesaplama.java
@@ -0,0 +1,58 @@
+package patika;
+
+import java.util.Scanner;
+
+public class ArtıkYilHesaplama {
+
+    /*
+            Java ile kullanıcının girdiği yılın artık olup olmadığını bulan programı yazınız.
+
+
+        Artık Yıl Nasıl Hesaplanır?
+
+        Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır:
+
+        1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
+        100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+
+        Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir.
+        Sadece 400'e tam olarak bölünebilenlerin artık yıl kabul edilmesinin nedeni,
+         bir astronomik yılın 365,25 gün değil, yaklaşık olarak 365,242 gün olmasından kaynaklanan hatayı gidermektir.
+
+        Senaryo
+
+        Yıl Giriniz : 2020
+        2020 bir artık yıldır !
+
+
+        Yıl Giriniz : 2021
+        2021 bir artık yıldır değildir !
+     */
+
+    public static void main(String[] args) {
+        int yil;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("bir yil giriniz");
+        yil=scan.nextInt();
+        
+        if (yil%100==0)
+        {
+            if (yil%400==0)
+            {
+                System.out.println("girdiginiz yil "+ yil+ " artik yildir");
+            }
+            else
+            {
+                System.out.println("girdiniz yil " + yil + " artik yil degildir");
+            }
+        }
+        else if (yil%4==0)
+        {
+            System.out.println("girdiginiz yil "+ yil+ " artik yildir");
+        }
+
+
+
+        
+    }
+}
