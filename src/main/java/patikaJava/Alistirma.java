cff75a4287fec89237b02205957e34091ca0acfb
rivrivsem <smrdnmz43@gmail.com>
2022-12-31 18:15:31 +0100

    duzenlendi

---
+++ src/main/java/patikaJava/Alistirma.java
@@ -0,0 +1,104 @@
+package patikaJava;
+
+public class Alistirma {
+
+
+//    Overloading ve Overriding anlatir misniz?
+
+//    overloading
+
+    static void yuklendim() {
+        System.out.println("acayip birseyim");
+    }
+
+    static void yuklendim(int a) {
+        System.out.println("bak bir parametrem var");
+    }
+
+    static int yuklendim(double a, double b) {
+        return (int) (a * b);
+    }
+
+    static float yuklendim(int a, float b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+//    overriding
+
+
+
+//    Functional Programming Language(Lambda) nedir, kisaca aciklar misiniz ?
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    static void show(int a, char b) {
+//        System.out.print("PATİKA PATİKA ");
+//    }
+//
+//    static void show(char a, int b) {
+//        System.out.print("DEV DEV ");
+//    }
+//
+//    public static void main(String[] args) {
+//        show(25, 'A');
+//        show('B', 11);
+//    }
+//}
+//
+
+
+//    static int info() {
+//        System.out.println("Patika");
+//        return 0;
+//    }
+//
+//    static void info() {
+//        System.out.println("Dev");
+//    }
+//
+//    public static void main(String[] args) {
+//        info();
+//    }
+//}
+
+
+//}
+//    static void print(){
+//        System.out.println("parametresiz");
+//    }
+//
+//    static void print(int a){
+//        System.out.println("parametreli :"+a);
+//
+//    }
+//    static int print(int a,int b,int c){
+//        return a+b-c;
+//    }
+//    static boolean print(int a,int b){
+//        return a<b;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(print(2, 3));
+//        System.out.println(print(2, 1));
+//        System.out.println(print(3, 5, 4));
+//    }
+
---
+++ src/main/java/patikaJava/Alistirma2.java
@@ -0,0 +1,30 @@
+package patikaJava;
+
+public class Alistirma2 {
+//    static void number(int x, int z) {
+//        int y = 2;
+//        x = x * y;
+//        System.out.println(x);
+//    }
+//    public static void main(String[] args) {
+//        int y = 3;
+//        int z = 2;
+//        if (y != z) {
+//            int x = 20;
+//        }
+//        int x = 50;
+//        number(x, z);
+//    }
+public static void main(String[] args) {
+
+
+    String x = "Patika.dev";
+    kodluyoruz(x);
+        System.out.println(x);
+}
+    static void kodluyoruz(String y)
+    {
+        String x = "Java101";
+        y = "Kodluyoruz";
+    }
+}
