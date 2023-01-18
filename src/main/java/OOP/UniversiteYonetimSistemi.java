653691746e18fade582347a45966ed47688edab7
rivrivsem <smrdnmz43@gmail.com>
2023-01-18 22:24:50 +0100

    universite yonetim sistemi diyagram

---
+++ src/main/java/OOP/UniversiteYonetimSistemi.java
@@ -0,0 +1,52 @@
+package OOP;
+
+public class UniversiteYonetimSistemi {
+
+    /*
+            Ödev - Üniversite Yönetim Sistemi
+
+            1 - Üniversiteye ait sınıflıklar, çalışma ofisleri ve departmanlar vardır.
+
+            2 - Departmanlara ait ofisler vardır.
+
+            3 - Üniversiteye ait çalışanlar vardır. Bu çalışanlar profesör veya memur olabilir.
+
+            4 - Her çalışan bir ofiste çalışır.
+
+            Bu sistemi tasvir eden Class (Sınıf) diyagramını çiziniz.
+
+            Not : Sınıflara ait nitelik ve davranışların belirtilmesine gerek yoktur.
+     */
+
+
+    /*
+
+
+                                   University
+                                       |
+                   +-------------------+-----------------+
+                   |                   |                 |
+                Offices             Departments       Employees
+                   |                   |                 |
+                   +                   +                 +
+                   |                   |                 |
+                Office             Department         Employee
+                                                             |
+                                                             +
+                                                             |
+                                                       Professor/Clerk
+
+
+
+
+
+
+
+
+
+
+
+
+
+     */
+}
