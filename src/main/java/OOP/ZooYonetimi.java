55e70f3508b89fd5bd25fd056d0710705b9e7259
rivrivsem <smrdnmz43@gmail.com>
2023-01-24 15:13:02 +0100

    Hayvanat Bahçesi Yönetimi diyagram eklendi

---
+++ src/main/java/OOP/ZooYonetimi.java
@@ -0,0 +1,40 @@
+package OOP;
+
+public class ZooYonetimi {
+
+    /*
+
+
+         ================================================
+                        [Animal] (abstract)
+                   - speciesName: string
+                   - weight: float
+                   - age: int
+
+                   + getDosage(): float
+                   + getFeedSchedule(): string
+              ======================================
+
+                  ===============================
+                    [Horse] (extends Animals)
+                    +getDosage(): float
+                    +getFeedSchedule(): string
+                 =================================
+
+                  =============================
+                    [Feline] (extends Animals)
+                    +getDosage(): float
+                    +getFeedSchedule(): string
+                   ============================
+
+                   ============================
+                    [Rodent] (extends Animals)
+                    +getDosage(): float
+                    +getFeedSchedule(): string
+                    ===========================
+
+
+                    etc...
+
+     */
+}
